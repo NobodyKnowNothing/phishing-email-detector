0568ef43502b41ba486fc4dbe9866f39b3b17fd5
EricSheridan3 <bertero@sheridancollege.ca>
2025-03-29 15:52:19 -0400

    Common commit

--- Gmail/main.py
+++ Gmail/main.py
@@ -1,10 +1,13 @@
 from google_api import init_gmail_service
-from Common import gmail_pharser
+from Common import gmail_pharser, extract_header_components
 
 if __name__ == '__main__':
     service = init_gmail_service()
-    list_response = service.users().messages().list(userId='me', labelIds=['INBOX'], maxResults=1).execute()
+    list_response = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
     if 'messages' in list_response:
-        message_id = list_response['messages'][0]['id']
-        gmail_pharser(message_id,service)
-        
+        for message in list_response.get('messages'):
+            message_id = message.get('id')
+            header = gmail_pharser(message_id,service)
+            
+            message_info = extract_header_components(header)
+            print(message_info)
